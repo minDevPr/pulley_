18e0a3f8846772c83d815b99befc4b2a8c003e84
min-96 <ajdcnd131@naver.com>
2024-11-06 22:41:22 +0900

    init

---
+++ src/main/kotlin/com/api/pully/PullyApplication.kt
@@ -0,0 +1,11 @@
+package com.api.pully
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PullyApplication
+
+fun main(args: Array<String>) {
+    runApplication<PullyApplication>(*args)
+}
